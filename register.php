e27e9c81153da89cdeeb6ee9691b39baaac83364
swap22 <sharma.sswapnil@gmail.com>
2017-05-30 16:14:57 +0530

    connection of the form is created with removal of html tag and trimming for name email

--- register.php
+++ register.php
@@ -1,3 +1,58 @@
+<?php
+    $con = mysqli_connect("localhost","root","","social");// checking connection with database by creating variable
+    if(mysqli_connect_errno()){
+        echo "Failed to connect to database".mysqli_connect_errno();
+    }
+
+    //Declaring variables to prevent errors
+        $fname = ""; //First name
+        $lname = ""; //Last name
+        $em = ""; //email
+        $em2 = ""; //email 2
+        $password = ""; //password
+        $password2 = ""; //password 2
+        $date = ""; //Sign up date 
+        $error_array = ""; //Holds error messages
+
+        //checking the submit button is pressed or not
+        if(isset($_POST['register_button'])){
+            //Registration form values
+
+            //First name
+            $fname = strip_tags($_POST['reg_fname']); //Remove html tags if user add it
+            $fname = str_replace(' ', '', $fname); //remove spaces placed before and after variable
+            $fname = ucfirst(strtolower($fname)); //Uppercase first letter and lower case the other
+
+
+            //Last name
+            $lname = strip_tags($_POST['reg_lname']);
+            $lname = str_replace(' ', '', $lname);
+            $lname = ucfirst(strtolower($lname)); 
+
+            //email address
+            $em = strip_tags($_POST['reg_email']);
+            $em = str_replace(' ', '', $em);
+            $em = ucfirst(strtolower($em)); 
+
+
+            //Confirm email
+            $em2 = strip_tags($_POST['reg_email2']); 
+            $em2 = str_replace(' ', '', $em2);
+            $em2 = ucfirst(strtolower($em2)); 
+
+
+            //Password
+            $password = strip_tags($_POST['reg_password']); 
+            $password2 = strip_tags($_POST['reg_password2']); 
+
+            $date = date("Y-m-d"); //Current date
+
+
+         }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
