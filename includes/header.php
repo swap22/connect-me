3c6c973c0a473937d9f8fdb9e64c97c800ae8959
swap22 <sharma.sswapnil@gmail.com>
2017-06-12 12:48:46 +0530

    add post to user on self wall

--- includes/header.php
+++ includes/header.php
@@ -54,7 +54,7 @@ else {
                     <a href="#">
                         <i class="fa fa-bell fa-lg"></i>
                     </a>
-                    <a href="#">
+                    <a href="requests.php">
                         <i class="fa fa-users fa-lg"></i>
                     </a>
                     <a href="#">
